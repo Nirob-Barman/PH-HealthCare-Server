7eec30d90a651c88bf43824fd5f277bfe4959f39
Nirob-Barman <niroborin111@gmail.com>
2024-11-07 09:17:43 +0600

    Optimization of Search Code: Part 1

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,16 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 // const getAllFromDb = async () => {
 const getAllFromDb = async (params: any) => {
     // console.log({ params });
-    // const result = await prisma.admin.findMany();
-    const result = await prisma.admin.findMany({
-        where: {
-            // name: {
-            //     contains: params.searchTerm,
-            //     mode: 'insensitive'
-            // }
+    const andConditions: Prisma.AdminWhereInput[] = [];
+
+    if (params.searchTerm) {
+        andConditions.push({
             OR: [
                 {
                     name: {
@@ -25,8 +22,43 @@ const getAllFromDb = async (params: any) => {
                     }
                 }
             ]
-        }
+        })
+    }
+    console.dir(andConditions, { depth: 'infinity' });
+
+    // const result = await prisma.admin.findMany();
+    // const result = await prisma.admin.findMany({
+    //     where: {
+    //         // name: {
+    //         //     contains: params.searchTerm,
+    //         //     mode: 'insensitive'
+    //         // }
+
+    //         OR: [
+    //             {
+    //                 name: {
+    //                     contains: params.searchTerm,
+    //                     mode: 'insensitive'
+    //                 }
+    //             },
+    //             {
+    //                 email: {
+    //                     contains: params.searchTerm,
+    //                     mode: 'insensitive'
+    //                 }
+    //             }
+    //         ]
+    //     }
+    // });
+
+    const whereConditions: Prisma.AdminWhereInput = {
+        AND: andConditions
+    }
+
+    const result = await prisma.admin.findMany({
+        where: whereConditions
     });
+
     return result
 }
 
