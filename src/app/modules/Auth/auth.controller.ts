e73d4038287149e7ec86edd64c0b0ef0d1d7f3ee
Nirob-Barman <niroborin111@gmail.com>
2024-11-21 08:48:06 +0600

    Sending Reset Email, Resetting Password

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -56,8 +56,36 @@ const changePassword = catchAsync(async (req: Request & { user?: any }, res: Res
     })
 });
 
+
+const forgotPassword = catchAsync(async (req: Request, res: Response) => {
+    await AuthServices.forgotPassword(req.body);
+
+    sendResponse(res, {
+        statusCode: StatusCodes.OK,
+        success: true,
+        message: "Check your email!",
+        data: null
+    })
+});
+
+const resetPassword = catchAsync(async (req: Request, res: Response) => {
+
+    const token = req.headers.authorization || "";
+
+    await AuthServices.resetPassword(token, req.body);
+
+    sendResponse(res, {
+        statusCode: StatusCodes.OK,
+        success: true,
+        message: "Password Reset!",
+        data: null
+    })
+});
+
 export const AuthController = {
     loginUser,
     refreshToken,
-    changePassword
+    changePassword,
+    forgotPassword,
+    resetPassword
 };
