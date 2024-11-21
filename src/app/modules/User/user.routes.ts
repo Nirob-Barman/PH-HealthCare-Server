e7dc5cdd6aae56690c869b67360e7823e10a2375
Nirob-Barman <niroborin111@gmail.com>
2024-11-21 07:51:03 +0600

    Creating Reusable Authentication Middleware and Handling API Errors

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -3,31 +3,32 @@ import { userController } from "./user.controller";
 import { jwtHelpers } from "../../../helpers/jwtHelpers";
 import config from "../../../config";
 import { Secret } from "jsonwebtoken";
+import auth from "../../middlewares/auth";
 
 const router = express.Router();
 
-const auth = (...roles: string[]) => {
-    return async (req: Request, res: Response, next: NextFunction) => {
-        // console.log(roles);
-        try {
-            const token = req.headers.authorization
-            // console.log(token);
-            if (!token) {
-                throw new Error("You are not authorized");
-            }
-            const verifiedUser = jwtHelpers.verifyToken(token, config.jwt.jwt_secret as Secret)
-            // console.log(verifiedUser);
+// const auth = (...roles: string[]) => {
+//     return async (req: Request, res: Response, next: NextFunction) => {
+//         // console.log(roles);
+//         try {
+//             const token = req.headers.authorization
+//             // console.log(token);
+//             if (!token) {
+//                 throw new Error("You are not authorized");
+//             }
+//             const verifiedUser = jwtHelpers.verifyToken(token, config.jwt.jwt_secret as Secret)
+//             // console.log(verifiedUser);
 
-            if (roles.length && !roles.includes(verifiedUser.role)) {
-                throw new Error("You are not authorized");
-            }
-            next()
-        }
-        catch (err) {
-            next(err)
-        }
-    }
-}
+//             if (roles.length && !roles.includes(verifiedUser.role)) {
+//                 throw new Error("You are not authorized");
+//             }
+//             next()
+//         }
+//         catch (err) {
+//             next(err)
+//         }
+//     }
+// }
 
 // router.get("/", (req: Request, res: Response) => {
 //     res.send({
