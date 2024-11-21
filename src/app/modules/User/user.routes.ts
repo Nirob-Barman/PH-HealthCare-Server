834bddd1e31ab83d0159e7ed62768245af4dc909
Nirob-Barman <niroborin111@gmail.com>
2024-11-21 07:42:29 +0600

    Implementing Authentication Middleware for Role-Based Access on APIs

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,7 +1,33 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { userController } from "./user.controller";
+import { jwtHelpers } from "../../../helpers/jwtHelpers";
+import config from "../../../config";
+import { Secret } from "jsonwebtoken";
 
 const router = express.Router();
+
+const auth = (...roles: string[]) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        // console.log(roles);
+        try {
+            const token = req.headers.authorization
+            // console.log(token);
+            if (!token) {
+                throw new Error("You are not authorized");
+            }
+            const verifiedUser = jwtHelpers.verifyToken(token, config.jwt.jwt_secret as Secret)
+            // console.log(verifiedUser);
+
+            if (roles.length && !roles.includes(verifiedUser.role)) {
+                throw new Error("You are not authorized");
+            }
+            next()
+        }
+        catch (err) {
+            next(err)
+        }
+    }
+}
 
 // router.get("/", (req: Request, res: Response) => {
 //     res.send({
@@ -9,6 +35,6 @@ const router = express.Router();
 //     })
 // });
 
-router.post("/", userController.createAdmin);
+router.post("/", auth("ADMIN", "SUPER_ADMIN"), userController.createAdmin);
 
 export const UserRoutes = router;
