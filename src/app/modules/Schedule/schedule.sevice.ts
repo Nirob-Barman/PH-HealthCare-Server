ff8c572a3e4b7225ea7b5aa909faf570251c07aa
Nirob-Barman <niroborin111@gmail.com>
2024-11-24 20:25:35 +0600

    Schedule creating

--- src/app/modules/Schedule/schedule.sevice.ts
+++ src/app/modules/Schedule/schedule.sevice.ts
@@ -1,5 +1,7 @@
+import { addHours, addMinutes, format } from 'date-fns';
 import { Schedule } from "@prisma/client";
 import { ISchedule } from "./schedule.interface";
+import prisma from '../../shared/prisma';
 
 
 const convertDateTime = async (date: Date) => {
@@ -19,56 +21,56 @@ const inserIntoDB = async (payload: ISchedule): Promise<Schedule[]> => {
 
     while (currentDate <= lastDate) {
         // 09:30  ---> ['09', '30']
-        // const startDateTime = new Date(
-        //     addMinutes(
-        //         addHours(
-        //             `${format(currentDate, 'yyyy-MM-dd')}`,
-        //             Number(startTime.split(':')[0])
-        //         ),
-        //         Number(startTime.split(':')[1])
-        //     )
-        // );
+        const startDateTime = new Date(
+            addMinutes(
+                addHours(
+                    `${format(currentDate, 'yyyy-MM-dd')}`,
+                    Number(startTime.split(':')[0])
+                ),
+                Number(startTime.split(':')[1])
+            )
+        );
 
-        // const endDateTime = new Date(
-        //     addMinutes(
-        //         addHours(
-        //             `${format(currentDate, 'yyyy-MM-dd')}`,
-        //             Number(endTime.split(':')[0])
-        //         ),
-        //         Number(endTime.split(':')[1])
-        //     )
-        // );
+        const endDateTime = new Date(
+            addMinutes(
+                addHours(
+                    `${format(currentDate, 'yyyy-MM-dd')}`,
+                    Number(endTime.split(':')[0])
+                ),
+                Number(endTime.split(':')[1])
+            )
+        );
 
-        // while (startDateTime < endDateTime) {
-        //     // const scheduleData = {
-        //     //     startDateTime: startDateTime,
-        //     //     endDateTime: addMinutes(startDateTime, interverlTime)
-        //     // }
+        while (startDateTime < endDateTime) {
+            // const scheduleData = {
+            //     startDateTime: startDateTime,
+            //     endDateTime: addMinutes(startDateTime, interverlTime)
+            // }
 
-        //     const s = await convertDateTime(startDateTime);
-        //     const e = await convertDateTime(addMinutes(startDateTime, interverlTime))
+            const s = await convertDateTime(startDateTime);
+            const e = await convertDateTime(addMinutes(startDateTime, interverlTime))
 
-        //     const scheduleData = {
-        //         startDateTime: s,
-        //         endDateTime: e
-        //     }
+            const scheduleData = {
+                startDateTime: s,
+                endDateTime: e
+            }
 
-        //     const existingSchedule = await prisma.schedule.findFirst({
-        //         where: {
-        //             startDateTime: scheduleData.startDateTime,
-        //             endDateTime: scheduleData.endDateTime
-        //         }
-        //     });
+            const existingSchedule = await prisma.schedule.findFirst({
+                where: {
+                    startDateTime: scheduleData.startDateTime,
+                    endDateTime: scheduleData.endDateTime
+                }
+            });
 
-        //     if (!existingSchedule) {
-        //         const result = await prisma.schedule.create({
-        //             data: scheduleData
-        //         });
-        //         schedules.push(result);
-        //     }
+            if (!existingSchedule) {
+                const result = await prisma.schedule.create({
+                    data: scheduleData
+                });
+                schedules.push(result);
+            }
 
-        //     startDateTime.setMinutes(startDateTime.getMinutes() + interverlTime);
-        // }
+            startDateTime.setMinutes(startDateTime.getMinutes() + interverlTime);
+        }
 
         currentDate.setDate(currentDate.getDate() + 1);
     }
