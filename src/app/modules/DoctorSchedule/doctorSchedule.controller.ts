d691783ff2c2a58409af629a7ee5cb5e2b75a5ca
Nirob-Barman <niroborin111@gmail.com>
2024-11-25 08:11:53 +0600

    Implementing Schedule Filtering

---
+++ src/app/modules/DoctorSchedule/doctorSchedule.controller.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+import catchAsync from "../../shared/catchAsync";
+import { IAuthUser } from "../../interfaces/common";
+import { DoctorScheduleService } from "./doctorSchedule.service";
+import sendResponse from "../../shared/sendResponse";
+import { StatusCodes } from "http-status-codes";
+import pick from "../../shared/pick";
+import { scheduleFilterableFields } from "./doctorSchedule.constants";
+
+const insertIntoDB = catchAsync(async (req: Request & { user?: IAuthUser }, res: Response) => {
+    const user = req.user;
+    const result = await DoctorScheduleService.insertIntoDB(user, req.body);
+    sendResponse(res, {
+        statusCode: StatusCodes.OK,
+        success: true,
+        message: "Doctor Schedule created successfully!",
+        data: result
+    });
+});
+
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+    const filters = pick(req.query, scheduleFilterableFields);
+    const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+    const result = await DoctorScheduleService.getAllFromDB(filters, options);
+    sendResponse(res, {
+        statusCode: StatusCodes.OK,
+        success: true,
+        message: 'Doctor Schedule retrieval successfully',
+        meta: result.meta,
+        data: result.data,
+    });
+});
+
+export const DoctorScheduleController = {
+    insertIntoDB,
+    // getMySchedule,
+    // deleteFromDB,
+    getAllFromDB
+};
